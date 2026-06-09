f4498c1090f0f64da70eca8c4f237e2ee6c481f9
JamesBirchall-dev <jamesbdorel@gmail.com>
2026-06-09 08:21:40 +0100

    Feature Test - anonymous access to reg page - sucess

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
+
+class AccountViewTests(TestCase):
+    """Tests for the account-related views."""
+
+    def test_register_page_loads_for_anonymous_user(self):
+        """Test that the registration page loads for anonymous users."""
+        response = self.client.get(reverse("register"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Register")
 # Create your tests here.
