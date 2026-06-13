74efaa1fd4e4652f5de0d56d553a75bc1aeb2cce
JamesBirchall-dev <jamesbdorel@gmail.com>
2026-06-13 14:23:27 +0100

    Tutor-Lesson CRUD - URL Paths: list, create, update, delete. urls.py

--- tutors/urls.py
+++ tutors/urls.py
@@ -10,9 +10,22 @@ from . import views
 app_name = 'tutors'
 
 urlpatterns = [
-    path('', views.tutor_list, name='tutor_list'),
-    path('<int:pk>/', views.tutor_detail, name='tutor_detail'),
-    path("create/", views.tutor_create, name="tutor_create"),
-    path("<int:pk>/edit/", views.tutor_update, name="tutor_update"),
-    path("<int:pk>/delete/", views.tutor_delete, name="tutor_delete"),
+    path('', views.tutor_list,
+         name='tutor_list'),
+    path('<int:pk>/', views.tutor_detail,
+         name='tutor_detail'),
+    path("create/", views.tutor_create,
+         name="tutor_create"),
+    path("<int:pk>/edit/", views.tutor_update,
+         name="tutor_update"),
+    path("<int:pk>/delete/", views.tutor_delete,
+         name="tutor_delete"),
+    path("<int:tutor_pk>/lessons/", views.lesson_list,
+         name="lesson_list"),
+    path("<int:tutor_pk>/lessons/create/", views.lesson_create,
+         name="lesson_create"),
+    path("<int:tutor_pk>/lessons/<int:pk>/edit/", views.lesson_update,
+         name="lesson_update"),
+    path("<int:tutor_pk>/lessons/<int:pk>/delete/", views.lesson_delete,
+         name="lesson_delete"),
 ]
